7d7596c7342b1fa37f0097c682f5f9e8095f9c14
Jaime <ajaime425@gmail.com>
2023-11-15 01:08:20 -0800

    Crafted a RecyclerView for Expenses, presenting individual DB entities and their distinct data

---
+++ app/src/main/java/com/example/expensetracker/ExpenseTrackerDb/ExpenseTrackerPreferences.java
@@ -0,0 +1,7 @@
+package com.example.expensetracker.ExpenseTrackerDb;
+
+public class ExpenseTrackerPreferences {
+//    public static final String SHARED_PREFS = "ExpenseTrackerPrefs";
+    public static final String LOGGED_IN = "isLoggedIn";
+    public static final String USER_ROLE = "userRole";
+}
